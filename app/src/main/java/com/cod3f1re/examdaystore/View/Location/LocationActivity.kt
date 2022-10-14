8f1e5fbbff2d53ef53fdb77203100828c68bf936
Abraham Rivera <cod4f1rex@gmail.com>
2022-10-14 18:14:18 -0500

    Inicio del examen

---
+++ app/src/main/java/com/cod3f1re/examdaystore/View/Location/LocationActivity.kt
@@ -0,0 +1,11 @@
+package com.cod3f1re.examdaystore.View.Location
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LocationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_location)
+    }
+}
